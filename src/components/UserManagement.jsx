e98fc6c08fd1823431db9e9e8c806e24b42395ef
camalmikayilov <camalmikayilov1@gmail.com>
2023-10-28 20:42:48 +0400

    update usermanagement

--- src/components/UserManagement.jsx
+++ src/components/UserManagement.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // UserManagement component
 const UserManagement = () => {
-
-  // Setting initial state for user values using useState hook
   const [uservalues, setUserValues] = useState({
     name: "",
     surname: "",
@@ -11,56 +9,77 @@ const UserManagement = () => {
     defaultPassword: "",
   });
 
-  // Handling form submission
+  const [addedUsers, setAddedUsers] = useState(
+    JSON.parse(localStorage.getItem("addedUsers")) || []
+  ); // State to store added users
+
+  useEffect(() => {
+    localStorage.setItem("addedUsers", JSON.stringify(addedUsers));
+  }, [addedUsers]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Retrieving and updating usersData from local storage
     var usersData = JSON.parse(localStorage.getItem("usersdata") || "[]");
     usersData.push(uservalues);
     localStorage.setItem("usersdata", JSON.stringify(usersData));
+    setAddedUsers([
+      ...addedUsers,
+      { firstName: uservalues.name, lastName: uservalues.surname },
+    ]);
+    setUserValues({ name: "", surname: "", email: "", defaultPassword: "" });
   };
 
-  // Handling user input changes
   const handleAddUser = (e) => {
     setUserValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  // Rendering the form
   return (
-    <form className="user-form" onSubmit={handleSubmit}>
-      {/* Input fields for user information */}
-      <input
-        type="text"
-        placeholder="Name"
-        onChange={handleAddUser}
-        className="user-input"
-        name="name"
-      />
-      <input
-        type="text"
-        placeholder="Surname"
-        onChange={handleAddUser}
-        className="user-input"
-        name="surname"
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={handleAddUser}
-        className="user-input"
-        name="email"
-      />
-      <input
-        type="password"
-        placeholder="Default Password"
-        onChange={handleAddUser}
-        className="user-input"
-        name="defaultPassword"
-      />
-      <button type="submit" className="user-button"> {/* Button for adding user */}
-        Add User
-      </button>
-    </form>
+    <div>
+      <form className="user-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={handleAddUser}
+          className="user-input"
+          name="name"
+        />
+        <input
+          type="text"
+          placeholder="Surname"
+          onChange={handleAddUser}
+          className="user-input"
+          name="surname"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={handleAddUser}
+          className="user-input"
+          name="email"
+        />
+        <input
+          type="password"
+          placeholder="Default Password"
+          onChange={handleAddUser}
+          className="user-input"
+          name="defaultPassword"
+        />
+        <button type="submit" className="user-button">
+          Add User
+        </button>
+      </form>
+
+      {/* Display added users */}
+      <div className="added-users">
+        {addedUsers.map((user, index) => (
+          <div key={index}>
+            <p>First Name: {user.firstName}</p>
+            <p>Last Name: {user.lastName}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
