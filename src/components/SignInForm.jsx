3f9f67b6ae531e77bfd2b4f9e154ef78ef23702d
camalmikayilov <camalmikayilov1@gmail.com>
2023-10-28 19:18:20 +0400

    update TaskManagement

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,44 +1,56 @@
 import { useState } from 'react';
 
 const SignInForm = () => {
-
   const [signivalues, setSignValues] = useState({
-  email: '',
-  password: ''
-  })
+    email: '',
+    password: ''
+  });
 
- const handleSubmit = (e) => {
+  const [showPopup, setShowPopup] = useState(false); // State for managing pop-up visibility
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-      var signInData  = JSON.parse(localStorage.getItem('signIndata') || "[]");
-      signInData.push(signivalues)
-      localStorage.setItem('signIndata', JSON.stringify(signInData))
-    }
-    
-    const handleSignIn = (e) => {
-      setSignValues(prev => ({...prev, [e.target.name]: [e.target.value] }))
-    };
+    var signInData = JSON.parse(localStorage.getItem('signIndata') || "[]");
+    signInData.push(signivalues);
+    localStorage.setItem('signIndata', JSON.stringify(signInData));
+    setShowPopup(true); // Show the pop-up on successful sign-in
+  };
 
- return (
-    <form className="signin-form" onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={handleSignIn}
-        className="signin-input"
-        name='email'
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={handleSignIn}
-        className="signin-input"
-        name='password'
-      />
-      <button type="submit" className="signin-button">
-        Sign In
-      </button>
-    </form>
- );
+  const handleSignIn = (e) => {
+    setSignValues((prev) => ({ ...prev, [e.target.name]: e.target.value })); // Fix the handleSignIn function
+  };
+
+  return (
+    <div>
+      <form className="signin-form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={handleSignIn}
+          className="signin-input"
+          name="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={handleSignIn}
+          className="signin-input"
+          name="password"
+        />
+        <button type="submit" className="signin-button">
+          Sign In
+        </button>
+      </form>
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <p>Successful Login!</p>
+            <button onClick={() => setShowPopup(false)}>Close</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default SignInForm;
--- src/components/TaskManagement.jsx
+++ src/components/TaskManagement.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const TaskManagement = () => {
   const [taskvalues, setTaskValues] = useState({
@@ -8,11 +8,27 @@ const TaskManagement = () => {
     stat: ''
   });
 
+  const [tasks, setTasks] = useState([]);
+  const [allTaskData, setAllTaskData] = useState([]);
+
+  useEffect(() => {
+    const taskData = JSON.parse(localStorage.getItem('taskdata')) || [];
+    setAllTaskData(taskData);
+    setTasks(taskData); // Set tasks from localStorage data
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    var taskData = JSON.parse(localStorage.getItem('taskdata')) || [];
-    taskData.push(taskvalues);
+    const taskData = [...allTaskData, taskvalues];
     localStorage.setItem('taskdata', JSON.stringify(taskData));
+    setAllTaskData(taskData);
+    setTasks(taskData); // Update tasks with the latest data
+    setTaskValues({
+      title: '',
+      description: '',
+      deadline: '',
+      stat: ''
+    });
   };
 
   const handleAddTask = (e) => {
@@ -20,39 +36,80 @@ const TaskManagement = () => {
     setTaskValues((prev) => ({ ...prev, [name]: value }));
   };
 
+  const handleDeleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+    setAllTaskData(updatedTasks);
+    localStorage.setItem('taskdata', JSON.stringify(updatedTasks));
+  };
+
+  const handleAddUser = () => {
+    // Add your logic here for adding a user
+    console.log("Add User logic here");
+  };
+
   return (
-    <form className="task-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Title"
-        onChange={handleAddTask}
-        className="task-input"
-        name="title"
-      />
-      <textarea
-        placeholder="Description"
-        onChange={handleAddTask}
-        className="task-input"
-        name="description"
-      ></textarea>
-      <input
-        type="text"
-        placeholder="Deadline"
-        onChange={handleAddTask}
-        className="task-input"
-        name="deadline"
-      />
-      <input
-        type="text"
-        placeholder="Status"
-        onChange={handleAddTask}
-        className="task-input"
-        name="stat"
-      />
-      <button type="submit" className="task-button">
-        Add Task
-      </button>
-    </form>
+    <div>
+        <form className="task-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          onChange={handleAddTask}
+          className="task-input"
+          name="title"
+          value={taskvalues.title}
+        />
+        <textarea
+          placeholder="Description"
+          onChange={handleAddTask}
+          className="task-input"
+          name="description"
+          value={taskvalues.description}
+        ></textarea>
+        <input
+          type="text"
+          placeholder="Deadline"
+          onChange={handleAddTask}
+          className="task-input"
+          name="deadline"
+          value={taskvalues.deadline}
+        />
+        <input
+          type="text"
+          placeholder="Status"
+          onChange={handleAddTask}
+          className="task-input"
+          name="stat"
+          value={taskvalues.stat}
+        />
+        <button type="submit" className="task-button">
+          Add Task
+        </button>
+      </form>
+      <div>
+        {tasks.length > 0 && (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task, index) => (
+                <tr key={index}>
+                  <td>{task.title}</td>
+                  <td>
+                    <button onClick={() => handleDeleteTask(index)} className="buttonDelete">Delete</button>
+                    <button onClick={handleAddUser} className="buttonAddUser">Add User</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
   );
 };
 
